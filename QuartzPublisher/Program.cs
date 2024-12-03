d54d28357b3f9402a7385c8390eef63f4d535b0a
chase <chase.bussey@pm.me>
2024-12-03 17:27:06 -0600

    feat: enable deletion of files no longer tagged for publication by default
    Refs: #1

--- QuartzPublisher/Program.cs
+++ QuartzPublisher/Program.cs
@@ -13,7 +13,7 @@ namespace QuartzPublisher
                 {
                     if (!Path.Exists(options.Source)) throw new ArgumentException("Source directory does not exist");
                     if (!Path.Exists(options.Destination)) throw new ArgumentException("Destination directory does not exist");
-                    PublishContent(options.Source, options.Destination, options.Verbose);
+                    PublishContent(options.Source, options.Destination, options.Verbose, options.NoDelete);
                 });
         }
 
@@ -24,18 +24,43 @@ namespace QuartzPublisher
         /// <param name="source"></param>
         /// <param name="destination"></param>
         /// <param name="verbose"></param>
-        private static void PublishContent(string source, string destination, bool verbose = false)
+        private static void PublishContent(string source, string destination, bool verbose = false, bool noDelete = false)
         {
             foreach (var file in Directory.GetFiles(source, "*.md", SearchOption.AllDirectories))
             {
                 if (verbose) Console.WriteLine($"Processing {file}");
-                if (!ShouldPublish(file, verbose)) continue;
+                if (!ShouldPublish(file)) continue;
                 
                 var fileDestination = Path.Join(destination, Path.GetFileName(file));
                 
                 if (verbose) Console.WriteLine($"Copying {file} to {fileDestination}");
                 File.Copy(file, fileDestination, true);
             }
+
+            if (noDelete)
+            {
+                if (!verbose) return;
+                
+                Console.WriteLine("Publishing complete.");
+                
+                return;
+            }
+            
+            if (verbose) Console.WriteLine($"Copying files to {destination} complete. Checking for files to delete.");
+            
+            foreach (var file in Directory.GetFiles(destination, "*.md", SearchOption.AllDirectories))
+            {
+                var sourceFile = Path.Join(source, Path.GetFileName(file));
+                if (ShouldPublish(sourceFile)) continue;
+                
+                if (verbose) Console.WriteLine($"Deleting {file}");
+                File.Delete(file);
+            }
+
+            if (!verbose) return;
+            
+            Console.WriteLine("Deletion complete.");
+            Console.WriteLine("Publishing complete.");
         }
         
         /// <summary>
@@ -44,7 +69,7 @@ namespace QuartzPublisher
         /// </summary>
         /// <param name="file"></param>
         /// <returns>true if file should be published, else false.</returns>
-        private static bool ShouldPublish(string file, bool verbose = false)
+        private static bool ShouldPublish(string file)
         {
             var shouldPublish = false;
             using var reader = new StreamReader(file);
@@ -61,7 +86,6 @@ namespace QuartzPublisher
                 if (string.Equals(parts[0], "publish", StringComparison.InvariantCultureIgnoreCase))
                 {
                     shouldPublish = bool.Parse(parts[1].Trim('"'));
-                    if (verbose) Console.WriteLine($"Publish: {parts[1].Trim('"')}, {shouldPublish}");
                 }
             }
 
@@ -83,5 +107,8 @@ namespace QuartzPublisher
         
         [Option('v', "verbose", Default = false, HelpText = "Enable verbose output")]
         public bool Verbose { get; set; }
+        
+        [Option("no-delete", Default = false, HelpText = "Disable deletion of files not marked for publishing")]
+        public bool NoDelete { get; set; }
     }
 }
