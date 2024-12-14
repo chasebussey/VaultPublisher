f809c2a8cf17a81288b14cb4a82aa24a2e9ade4a
Chase Bussey <chase.bussey@pm.me>
2024-12-13 18:12:05 -0600

    fix: improve output, both verbose and non-verbose

--- VaultPublisher/Commands/PublishCommandHandler.cs
+++ VaultPublisher/Commands/PublishCommandHandler.cs
@@ -12,28 +12,50 @@ public static class PublishCommandHandler
     /// <param name="noDelete"></param>
     public static void PublishContent(string source, string destination, bool verbose = false, bool noDelete = false)
     {
+        if (verbose) Console.WriteLine($"Beginning publication of files from {source} to {destination}");
+        
+        var publishedFiles = new List<string>();
         foreach (var file in Directory.GetFiles(source, "*.md", SearchOption.AllDirectories))
         {
-            if (verbose) Console.WriteLine($"Processing {file}");
+            if (verbose) Console.WriteLine($"Checking {file} for publication frontmatter.");
             if (!ShouldPublish(file)) continue;
 
             var fileDestination = Path.Join(destination, Path.GetFileName(file));
 
             if (verbose) Console.WriteLine($"Copying {file} to {fileDestination}");
             File.Copy(file, fileDestination, true);
+            publishedFiles.Add(fileDestination);
+        }
+
+        if (verbose && !noDelete)
+        {
+            Console.WriteLine("Publication complete.");
+            WriteSeparator();
         }
 
         if (noDelete)
         {
-            if (!verbose) return;
-
-            Console.WriteLine("Publishing complete.");
-
+            if (verbose)
+            {
+                Console.WriteLine("Skipping deletion of files not marked for publishing.");
+                WriteSeparator();
+    
+                Console.WriteLine("Published files: ");
+                foreach (var file in publishedFiles)
+                {
+                    Console.WriteLine(file);
+                }
+                WriteSeparator();
+            }
+            
+            Console.WriteLine($"Published {publishedFiles.Count} files to {destination}");
+            
             return;
         }
 
-        if (verbose) Console.WriteLine($"Copying files to {destination} complete. Checking for files to delete.");
-
+        if (verbose) Console.WriteLine($"Checking for files to delete.");
+        
+        var deletedFiles = new List<string>();
         foreach (var file in Directory.GetFiles(destination, "*.md", SearchOption.AllDirectories))
         {
             var sourceFile = Path.Join(source, Path.GetFileName(file));
@@ -41,12 +63,30 @@ public static class PublishCommandHandler
 
             if (verbose) Console.WriteLine($"Deleting {file}");
             File.Delete(file);
+            deletedFiles.Add(file);
         }
 
-        if (!verbose) return;
+        if (verbose)
+        {
+            Console.WriteLine("Deletion complete.");
+            WriteSeparator();
+            
+            Console.WriteLine("Published files: ");
+            foreach (var file in publishedFiles)
+            {
+                Console.WriteLine(file);
+            }
+            WriteSeparator();
 
-        Console.WriteLine("Deletion complete.");
-        Console.WriteLine("Publishing complete.");
+            Console.WriteLine("Deleted files: ");
+            foreach (var file in deletedFiles)
+            {
+                Console.WriteLine(file);
+            }
+            WriteSeparator();
+        }
+        
+        Console.WriteLine($"Published {publishedFiles.Count} files and deleted {deletedFiles.Count} files from {destination}");
     }
 
     /// <summary>
@@ -57,6 +97,8 @@ public static class PublishCommandHandler
     /// <returns>true if file should be published, else false.</returns>
     private static bool ShouldPublish(string file)
     {
+        if (!File.Exists(file)) return false;
+        
         var shouldPublish = false;
         using var reader = new StreamReader(file);
         var inFrontmatter = false;
@@ -77,4 +119,6 @@ public static class PublishCommandHandler
 
         return shouldPublish;
     }
+    
+    private static void WriteSeparator() => Console.WriteLine(new string('-', 50));
 }
