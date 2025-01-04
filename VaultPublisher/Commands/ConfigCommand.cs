c5dd434586feaa44b4267c69f1e96a56787c8d06
Chase Bussey <chase.bussey@pm.me>
2025-01-03 19:56:37 -0600

    feat: add config list command, remove that functionality from config get

--- VaultPublisher/Commands/ConfigCommand.cs
+++ VaultPublisher/Commands/ConfigCommand.cs
@@ -8,19 +8,29 @@ internal static class ConfigCommand
     public static Command Create()
     {
         var command = new Command("config", "Manage configuration settings");
-        
+
+        command.AddCommand(BuildListCommand());
         command.AddCommand(BuildGetCommand());
         command.AddCommand(BuildSetCommand());
         command.AddCommand(BuildRemoveCommand());
 
         return command;
     }
+
+    private static Command BuildListCommand()
+    {
+        var command = new Command("list", "List all configuration settings");
+        
+        command.SetHandler(ConfigCommandHandler.List, new ConfigurationProvider());
+        
+        return command;
+    }
     
     private static Command BuildGetCommand()
     {
-        var command = new Command("get", "Get a configuration setting. If no key is provided, all settings are returned.");
+        var command = new Command("get", "Get a configuration setting by key.");
 
-        var keyArg = new Argument<string?>(name: "key", description: "The key of the configuration setting", getDefaultValue: () => null);
+        var keyArg = new Argument<string>(name: "key", description: "The key of the configuration setting");
         command.AddArgument(keyArg);
         
         command.SetHandler(ConfigCommandHandler.Get, keyArg, new ConfigurationProvider());
--- VaultPublisher/Commands/ConfigCommandHandler.cs
+++ VaultPublisher/Commands/ConfigCommandHandler.cs
@@ -4,28 +4,26 @@ namespace VaultPublisher.Commands;
 
 internal static class ConfigCommandHandler
 {
-    public static void Get(string? key, IConfiguration config)
+    public static void List(IConfiguration config)
     {
-        if (string.IsNullOrEmpty(key))
+        foreach (var setting in config.AsEnumerable())
         {
-            foreach (var setting in config.AsEnumerable())
-            {
-                Console.WriteLine($"{setting.Key}: {setting.Value}");
-            }
-        }
-        else
-        {
-            var value = config[key];
-            Console.WriteLine($"{key}: {value}");
+            Console.WriteLine($"{setting.Key}: {setting.Value}");
         }
     }
-    
+
+    public static void Get(string? key, IConfiguration config)
+    {
+        var value = config[key];
+        Console.WriteLine($"{key}: {value}");
+    }
+
     public static void Set(string key, string value, IConfiguration config)
     {
         config[key] = value;
         ConfigurationProvider.Save(config);
     }
-    
+
     public static void Remove(string key, IConfiguration config)
     {
         config[key] = null;
